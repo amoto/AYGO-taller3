e53ed0217a10d461c27dc88e07d94cc64495667b
julian david devia serna <judadese@gmail.com>
2021-11-19 22:31:57 -0500

    front test

--- front/src/config.js
+++ front/src/config.js
@@ -5,7 +5,7 @@ const config = {
     "aws_user_pools_web_client_id": "6tgppj5t2g4sv852tjbe05bffv",     // CognitoClientID
     "api_base_url": "None",                                     // TodoFunctionApi
     "cognito_hosted_domain": "aygo-taller3.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-    "redirect_url": "http://localhost:3000"                                      // AmplifyURL
+    "redirect_url": "https://master.d36xp2z3xbppz9.amplifyapp.com/"                                      // AmplifyURL
 };
 
 export default config;
